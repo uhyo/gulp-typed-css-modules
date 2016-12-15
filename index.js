22764e3e41756b73ed54b572c71a5268056fd58f
uhyo <uhyo@uhyohyo.net>
2016-12-15 20:35:21 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const through2 = require('through2');
+const gutil = require('gulp-util');
+const bufferstreams = require('bufferstreams');
+
+const pluginName = 'gulp-typed-css-modules';
+
+module.exports = options=>{
+    options = options || {};
+
+    const DtsCreator = options.ctm || require('typed-css-modules');
+
+    const creator = new DtsCreator(options);
+
+    return through2.obj((file, encoding, callback)=>{
+        const filepath = file.path;
+        const newpath = gutil.replaceExtension(filepath, '.d.ts');
+
+        file.path = newpath;
+        if (file.isNull()){
+            callback(null);
+            return;
+        }
+        if (file.isBuffer()){
+            runtcm(filepath, creator, file.contents.toString(encoding), options)
+            .then(content=>{
+                file.contents = content;
+                callback(null, file);
+            })
+            .catch(err=>{
+                callback(null);
+            });
+            return;
+        }
+        if (file.isStream()){
+            file.contents = file.contents.pipe(new bufferstreams((err, buf, callback)=>{
+                if (err){
+                    throw err;
+                }
+
+                runtcm(filepath, creator, buf.toString(encoding), options)
+                .then(content=>{
+                    callback(null, content);
+                })
+                .catch(err=>{
+                    callback(err);
+                });
+            }));
+
+            callback(null, file);
+            return;
+        }
+        callback(null);
+    });
+};
+
+function showWarning(file, ...err){
+    gutil.log(gutil.colors.cyan(pluginName), gutil.colors.yellow('Warning'), gutil.colors.gray(file), ...err);
+}
+function showError(...err){
+    gutil.log(gutil.colors.cyan(pluginName), gutil.colors.red('Error'), gutil.colors.gray(file), ...err);
+}
+
+function runtcm(filepath, creator, content, options){
+    return creator.create(filepath, content)
+    .then(content=>{
+        if (!options.quiet){
+            for (let mes of content.messageList){
+                showWarning(filepath, mes);
+            }
+        }
+        return new Buffer(content.formatted);
+    })
+    .catch(err=>{
+        showError(filepath, err);
+        return Promise.reject(err);
+    });
+}
